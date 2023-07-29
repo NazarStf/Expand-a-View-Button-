a9b323b4cc2f3048d00035e832f497ee96c686c4
NazarStf <hworker98@gmail.com>
2023-07-29 19:18:23 +0300

    Initial Commit

---
+++ Expand a View/Expand_a_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expand_a_ViewApp.swift
+//  Expand a View
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Expand_a_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
